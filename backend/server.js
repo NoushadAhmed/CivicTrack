7698bdeb688418aa386595ff4edfab4f8820e37e
Abhisheksingh1124 <abhisheksinghtopannavar@gmail.com>
2026-06-15 20:48:03 +0530

    add env test

--- backend/server.js
+++ backend/server.js
@@ -15,23 +15,23 @@ const app = express();
 
 // Middleware
 app.use(
-cors({
-origin: [
-"http://localhost:5173",
-"https://civic-track-k6dodxiku-noushadahmeds-projects.vercel.app",
-],
-credentials: true,
-})
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://civic-track-k6dodxiku-noushadahmeds-projects.vercel.app",
+        ],
+        credentials: true,
+    })
 );
 
 app.use(express.json());
 
 app.use(
-session({
-secret: process.env.JWT_SECRET,
-resave: false,
-saveUninitialized: false,
-})
+    session({
+        secret: process.env.JWT_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
 );
 
 app.use(passport.initialize());
@@ -42,12 +42,21 @@ app.use("/api/complaints", complaintRoutes);
 
 // Home Route
 app.get("/", (req, res) => {
-res.send("Community Complaint Management API Running");
+    res.send("Community Complaint Management API Running");
 });
 
 // Server
 const PORT = process.env.PORT || 5000;
 
+app.get("/env-test", (req, res) => {
+    res.json({
+        googleClientId: !!process.env.GOOGLE_CLIENT_ID,
+        googleClientSecret: !!process.env.GOOGLE_CLIENT_SECRET,
+        jwtSecret: !!process.env.JWT_SECRET,
+        mongoUri: !!process.env.MONGO_URI,
+    });
+});
+
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
